b0346a09344b3161a0507e3e893e20a8ab1b7cba
kusi <lawcubegsm@gmail.com>
2022-08-23 16:25:44 +0000

    Add task 1

--- stringlength.js
+++ stringlength.js
@@ -1,6 +1,12 @@
 
 const stringLength = (string) =>{
- return string.length
+
+  if (string.length >= 1 && string.length <= 10){
+   return string.length
+  }
+  // throw 'String length must be between 0 and 11'
+ return 'String length must be between 0 and 11';
+
 }
 
 
